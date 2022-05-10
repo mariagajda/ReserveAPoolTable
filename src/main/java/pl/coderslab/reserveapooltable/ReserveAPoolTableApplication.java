2d1d749e93b5b795c94ced1d72f88f7239b26472
mariagajda <maria.e.gajda@gmail.com>
2022-05-10 13:09:49 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/reserveapooltable/ReserveAPoolTableApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.reserveapooltable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveAPoolTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveAPoolTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/reserveapooltable/ReserveAPoolTableApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.reserveapooltable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveAPoolTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
